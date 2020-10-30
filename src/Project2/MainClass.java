df6bbaa28c2bee8e9779f975637cdff1929889bd
Didityuliyanto <57521755+Didityuliyanto@users.noreply.github.com>
2020-10-30 19:36:27 +0700

    Add files via upload

---
+++ src/Project2/MainClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //
+        Game2 x = new Game2();
+        x.input();
+        x.output();
+    }
+}
